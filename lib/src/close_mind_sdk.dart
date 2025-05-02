f0d2bf24e0d1bb6943e34dcead4208afa07a229b
pefmedina <pef.medina@gmail.com>
2025-05-02 16:33:20 -0300

    fix: library change with show command

--- lib/src/close_mind_sdk.dart
+++ lib/src/close_mind_sdk.dart
@@ -1,10 +1,10 @@
 library close_mind_sdk;
 
 // Exporte a classe CloseMind (agora em src/close_mind.dart)
-export 'package:close_mind_sdk/src/close_mind.dart';
+export 'package:close_mind_sdk/src/close_mind.dart' show CloseMind;
 
 // Exporte a enum CloseMindEnvironment e a classe CloseMindOptions
-export 'package:close_mind_sdk/src/close_mind_options.dart';
+export 'package:close_mind_sdk/src/close_mind_options.dart' show CloseMindEnvironment, CloseMindOptions;
 
 // Exporte a exceção customizada
-export 'package:close_mind_sdk/src/exceptions/close_mind_exception.dart';
+export 'package:close_mind_sdk/src/exceptions/close_mind_exception.dart' show CloseMindException;
